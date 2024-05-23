e17eaba5747abe7fc02c4de9b8e98f70238672e3
Derya AY <derya.ay26@hotmail.com>
2024-05-23 16:13:07 +0200

    init project

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    backendClient : '/assets/mock/product.json',
+    backendLoginClient: '/api/utilisateur/login',
+    backendCatalogue: '/api/catalogue'
+  };
